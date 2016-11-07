1da5f02777141f994b675ed623b3557554c1b8c1
Ethan Smith <ethan@onebytegone.com>
2016-11-07 12:21:24 -0500

    Add LED toggle command

--- SerialLolShield.ino
+++ SerialLolShield.ino
@@ -1,10 +1,15 @@
 /**
  * Serial LoL Shield Test
  *
+ * Dependencies:
+ * - https://github.com/jprodgers/LoLshield
+ *
  * Created by Ethan Smith on 2016/11/4
  */
 
-#define MESSAGE_MAX_LENGTH 7  // e.g. T00001
+#define MESSAGE_MAX_LENGTH 9  // e.g. T 10 15 1
+
+#include <Charliplexing.h>  // The LoLShield library
 
 char serialBuffer[MESSAGE_MAX_LENGTH];
 const char NO_MESSAGE = '\0';
@@ -15,6 +20,8 @@ void setup() {
    Serial.println("powering up...");
 
    resetSerialBuffer();
+
+   LedSign::Init(GRAYSCALE);
 }
 
 void loop() {
@@ -25,6 +32,8 @@ void loop() {
       Serial.print("processing [");
       Serial.print(message);
       Serial.println("]");
+
+      processCommand(message);
    }
 }
 
@@ -54,4 +63,46 @@ const char* fetchSerialMessage() {
 
 void resetSerialBuffer() {
    serialBufferIndex = 0;
+}
+
+void processCommand(const char* command) {
+   switch (command[0]) {
+      case 't':
+      case 'T':
+         handleToggleLEDCommand(&command[1]);
+         break;
+      default:
+         Serial.println("invalid command");
+         break;
+   }
+}
+
+void handleToggleLEDCommand(const char* buffer) {
+   char *fieldPointer;
+   uint8_t x, y, level;
+
+   x = (uint8_t)strtol(buffer, &fieldPointer, 10);
+   y = (uint8_t)strtol(fieldPointer, &fieldPointer, 10);
+   level = (uint8_t)strtol(fieldPointer, &fieldPointer, 10);
+
+   toggleLED(x, y, level);
+}
+
+void toggleLED(uint8_t x, uint8_t y, uint8_t level) {
+   x = bound(x, 0, DISPLAY_COLS - 1);
+   y = bound(y, 0, DISPLAY_ROWS - 1);
+   level = bound(level, 0, SHADES - 1);
+
+   Serial.print("setting led (");
+   Serial.print(x);
+   Serial.print(",");
+   Serial.print(y);
+   Serial.print(") to: ");
+   Serial.println(level);
+
+   LedSign::Set(x, y, level);
+}
+
+uint8_t bound(uint8_t value, uint8_t min, uint8_t max) {
+   return max(min(value, max), min);
 }
